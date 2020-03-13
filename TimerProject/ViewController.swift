2c225a72fea4d967c132d11c30d145f154622d1e
Faridho <faridho@zeepos.com>
2020-03-13 23:28:32 +0700

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Faridho on 13/03/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
